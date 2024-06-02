50fbcb247c640e00fc6c5369aa85655bacef2c7a
AssaneDev <assdev@gmail.com>
2024-06-02 14:17:35 +0000

    test modification

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -159,7 +159,28 @@
 		 {{-- End DataTable JS --}}
 
 		   {{-- TyniText Editor --}}
-	 <script src="https://cdn.tiny.cloud/1/86rr80i272jvqoq70sl2bxtoo3ntpqozqnamhim37gh1l4qg/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+	 {{-- <script src="https://cdn.tiny.cloud/1/86rr80i272jvqoq70sl2bxtoo3ntpqozqnamhim37gh1l4qg/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script> --}}
+	 <!-- Place the first <script> tag in your HTML's <head> -->
+		<script src="https://cdn.tiny.cloud/1/ncgg3kfk200ae0f3hax0io2deq63iv2afbadbbdkgo0ymmn4/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+
+<!-- Place the following <script> and <textarea> tags your HTML's <body> -->
+<script>
+  tinymce.init({
+    selector: 'textarea',
+    plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+    mergetags_list: [
+      { value: 'First.Name', title: 'First Name' },
+      { value: 'Email', title: 'Email' },
+    ],
+    ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+  });
+</script>
+{{-- <textarea>
+  Welcome to TinyMCE!
+</textarea>
 	 <script>
 	 tinymce.init({
 		 selector: 'textarea#myeditorinstance', // Replace this CSS selector to match the placeholder element for TinyMCE
@@ -171,7 +192,25 @@
 	 <script type="text/javascript" src="node_modules/froala-editor/js/froala_editor.pkgd.min.js"></script>
 	 <script> 
 		var editor = new FroalaEditor('#example');
-	</script>
+	</script> --}}
+
+	<script>
+		tinymce.init({
+		  selector: 'textarea',
+		  plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+		  toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+		  tinycomments_mode: 'embedded',
+		  tinycomments_author: 'Author name',
+		  mergetags_list: [
+			{ value: 'First.Name', title: 'First Name' },
+			{ value: 'Email', title: 'Email' },
+		  ],
+		  ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+		});
+	  </script>
+	  <textarea>
+		Welcome to TinyMCE!
+	  </textarea>
 </body>
 
 </html>
