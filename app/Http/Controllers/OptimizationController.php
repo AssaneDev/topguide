035a5eae73d5ad38284eaa905964780f74cd0a03
AssaneDev <assdev@gmail.com>
2024-07-21 13:18:29 +0000

    maj

--- app/Http/Controllers/OptimizationController.php
+++ app/Http/Controllers/OptimizationController.php
@@ -10,15 +10,26 @@ class OptimizationController extends Controller
     //
     public function optimize()
     {
-        // Exécute la commande artisan optimize
-        Artisan::call('optimize');
+     
 
-        // Optionnel : Retourne un message de succès ou redirige l'utilisateur
-        $notification = array(
-            'message' =>'Cache nettoyer avec succes',
-            'alert-type' => 'success'
-        );
-        
-        return  redirect()->back()->with($notification);
+
+        if (is_writable(storage_path()) && is_writable(base_path('bootstrap/cache'))) {
+            // Exécute la commande artisan optimize
+            Artisan::call('optimize');
+
+            // Retourne un message de succès
+              // Optionnel : Retourne un message de succès ou redirige l'utilisateur
+                $notification = array(
+                'message' =>'Cache nettoyer avec succes',
+                'alert-type' => 'success'
+            );
+            
+            return  redirect()->back()->with($notification);
+        } else {
+            // Retourne un message d'erreur
+            return response()->json(['message' => 'Permissions insuffisantes sur les dossiers nécessaires.'], 500);
+        }
+
+     
     }
 }
